0feea099dcbec97e4dedea3ecdcfc549e5782fe7
Asira Ratnayake <asiraratnayake@gmail.com>
2026-01-16 11:31:56 +0530

    dd

--- public/config.js
+++
@@ -1,3 +0,0 @@
-// globalThis.api_uri = "https://api.sirimalanka.com";
-globalThis.api_uri = "http://18.143.240.114:8080";
-// globalThis.api_uri = "http://localhost:8080";p
--- src/api/apiConfig.js
+++
@@ -1,1 +0,0 @@
-export const API_URI = globalThis.api_uri;
